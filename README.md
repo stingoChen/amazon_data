688a1d51f396e421f7a2d98062cfb2f359920a24
chenxinke <chenxinke@cai-inc.com>
2024-12-25 11:42:03 +0800

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # amazon_data
+
+# 安装  
+pip install -r requirement.txt
+
+# 使用
+1. 创建txt文件夹,文件夹名称为今日日期. 例如 [2024-12-25.txt]  
+2. txt中每一行为ASIN链接.  
+3. python main.py
+
--- main.py
+++ main.py
@@ -1,3 +1,37 @@
+from datetime import datetime
+
+import pandas as pd
+from playwright.sync_api import sync_playwright
 from src.pipeline import scrape_website
+from tqdm import tqdm
 
+if __name__ == '__main__':
+    now = datetime.now().strftime('%Y-%m-%d')
 
+    with open(f"{now}.txt", "r") as f:
+        datas = f.readlines()
+        datas = [_.strip() for _ in datas]
+
+    df = {}
+
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=True)
+        browser_context = browser.new_context(
+            user_agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.37",
+            locale = "en-US",
+            timezone_id = "America/New_York"
+        )
+        for idx, _ in tqdm(enumerate(datas), total=len(datas)):
+            r = scrape_website(_, browser_context)
+
+            if idx == 0:
+                df = {k: [v] for k, v in r.__dict__.items()}
+            else:
+                for k, v in r.__dict__.items():
+                    df[k].append(v)
+            # break
+        browser.close()
+
+    df = pd.DataFrame(df)
+
+    df.to_excel(f"{now}.xlsx", index=False)
